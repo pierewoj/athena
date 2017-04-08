b0f1f66e3faa65badfbaa704276643dcab89fd29
pierewoj <kuba.pierewoj@gmail.com>
2017-04-09 01:28:42 +0200

    usuniecie filtrowania linkow

--- Athena.Api/Athena.Api/Infrastructure/Services/IShouldCrawlDecider.cs
+++ Athena.Api/Athena.Api/Infrastructure/Services/IShouldCrawlDecider.cs
@@ -15,26 +15,7 @@ namespace Athena.Api.Infrastructure.Services
     {
         public bool ShouldCrawl(string page)
         {
-            return
-                page.StartsWith("http://kwestiasmaku.com")
-                && IsValidUri(page)
-                && !page.Contains("taxonomy")
-                && !page.Contains("sort_by")
-                && !page.Contains("login")
-                && !page.Contains("user");
-        }
-
-        public bool IsValidUri(string page)
-        {
-            try
-            {
-                var uri = new Uri(page);
-                return true;
-            }
-            catch(Exception)
-            {
-                return false;
-            }
+            return true;
         }
     }
 }
